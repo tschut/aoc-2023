23e213ba5808ebb07008eecfe8a54b30672af0d4
Tiemen Schut <tschut@gmail.com>
2023-12-01 10:27:54 +0100

    Day1

--- src/main/kotlin/nl/tiemenschut/aoc/y2023/day1.kt
+++ src/main/kotlin/nl/tiemenschut/aoc/y2023/day1.kt
@@ -2,15 +2,39 @@ package nl.tiemenschut.aoc.y2023
 
 import nl.tiemenschut.aoc.lib.dsl.aoc
 import nl.tiemenschut.aoc.lib.dsl.day
+import nl.tiemenschut.aoc.lib.dsl.parser.AsListOfStrings
 
 fun main() {
-    aoc {
+    aoc(AsListOfStrings) {
         puzzle { 2023 day 1 }
 
-        part1(submit = false) { input ->
+        part1 { input ->
+            input.sumOf { line ->
+                val numbers = line.filter { it.isDigit() }
+                "${numbers.first()}${numbers.last()}".toInt()
+            }
         }
 
-        part2(submit = false) { input ->
+        val digits = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+        )
+
+        part2 { input ->
+            input.sumOf { line ->
+                val numbers = line.windowed(size = 5, partialWindows = true).mapNotNull {
+                    if (it.first().isDigit()) it.first().digitToInt()
+                    else digits[digits.keys.firstOrNull { digit -> it.startsWith(digit) }]
+                }
+                "${numbers.first()}${numbers.last()}".toInt()
+            }
         }
     }
 }
