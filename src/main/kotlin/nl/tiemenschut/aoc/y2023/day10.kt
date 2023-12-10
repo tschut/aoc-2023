bcf487f8f6811c0866d2f1b546333b1b03b887f1
Tiemen Schut <tschut@gmail.com>
2023-12-10 11:01:30 +0100

    Day10 part2

--- src/main/kotlin/nl/tiemenschut/aoc/y2023/day10.kt
+++ src/main/kotlin/nl/tiemenschut/aoc/y2023/day10.kt
@@ -7,35 +7,35 @@ import nl.tiemenschut.aoc.lib.util.points.Point
 import nl.tiemenschut.aoc.lib.util.points.PointInt
 import nl.tiemenschut.aoc.lib.util.points.by
 
-class TubeMap(
-    val grid: List<List<Char>>,
-    val start: PointInt
+data class TubeMap(
+    val grid: List<MutableList<Char>>,
+    val start: PointInt,
 ) {
     fun findConnections(p: Point<Int>): List<Point<Int>> = buildList {
-        if (grid[p].connectsUp() && grid[p.up()].connectsDown()) add(p.up())
-        if (grid[p].connectsDown() && grid[p.down()].connectsUp()) add(p.down())
-        if (grid[p].connectsLeft() && grid[p.left()].connectsRight()) add(p.left())
-        if (grid[p].connectsRight() && grid[p.right()].connectsLeft()) add(p.right())
+        if (get(p).connectsUp() && get(p.up()).connectsDown()) add(p.up())
+        if (get(p).connectsDown() && get(p.down()).connectsUp()) add(p.down())
+        if (get(p).connectsLeft() && get(p.left()).connectsRight()) add(p.left())
+        if (get(p).connectsRight() && get(p.right()).connectsLeft()) add(p.right())
     }
 
-    operator fun List<List<Char>>.get(p: Point<Int>): Char = this[p.x][p.y]
-
-    private fun Char.connectsUp() = this in listOf('S', '|', 'L', 'J')
-    private fun Char.connectsDown() = this in listOf('S', '|', 'F', '7')
-    private fun Char.connectsLeft() = this in listOf('S', '-', 'J', '7')
-    private fun Char.connectsRight() = this in listOf('S', '-', 'F', 'L')
+    fun get(p: Point<Int>): Char = grid[p.x][p.y]
 }
+
+fun Char.connectsUp() = this in listOf('S', '|', 'L', 'J')
+fun Char.connectsDown() = this in listOf('S', '|', 'F', '7')
+fun Char.connectsLeft() = this in listOf('S', '-', 'J', '7')
+fun Char.connectsRight() = this in listOf('S', '-', 'F', 'L')
 
 object CharGridParser : InputParser<TubeMap> {
     override fun parse(input: String): TubeMap {
         val lines = input.split("\n")
 
         var start = 0 by 0
-        val grid = List(lines[0].length) { MutableList(lines.size) { '.' } }.also { grid ->
+        val grid = List(lines[0].length + 2) { MutableList(lines.size + 2) { '.' } }.also { grid ->
             for (y in lines.indices) {
                 for (x in lines[y].indices) {
-                    grid[x][y] = lines[y][x]
-                    if (grid[x][y] == 'S') start = x by y
+                    grid[x + 1][y + 1] = lines[y][x]
+                    if (grid[x + 1][y + 1] == 'S') start = x by y
                 }
             }
         }
@@ -54,8 +54,7 @@ fun main() {
                 val newConnections = connections.filter { it !in seen }.toSet()
                 return if (newConnections.size == 1) {
                     distance
-                }
-                else {
+                } else {
                     findDistance(newConnections.first(), newConnections.last(), seen + newConnections, distance + 1)
                 }
             }
@@ -63,7 +62,62 @@ fun main() {
             findDistance(input.start, input.start, mutableSetOf(input.start), 1)
         }
 
-        part2(submit = false) { input ->
+        part2 { input ->
+            tailrec fun findLoop(c1: Point<Int>, c2: Point<Int>, loop: Set<Point<Int>>): Set<Point<Int>> {
+                val connections = input.findConnections(c1) + input.findConnections(c2)
+                val newConnections = connections.filter { it !in loop }.toSet()
+                return if (newConnections.size == 1) {
+                    newConnections + loop
+                } else {
+                    findLoop(newConnections.first(), newConnections.last(), loop + newConnections)
+                }
+            }
+
+            val loop = findLoop(input.start, input.start, mutableSetOf(input.start))
+            val gridWithLoop = input.copy(grid = input.grid.mapIndexed { x, line ->
+                line.mapIndexed { y, c -> if (x by y in loop) c else '.' }.toMutableList()
+            })
+
+            val expandedGrid = gridWithLoop.copy(
+                grid = List(gridWithLoop.grid.size * 2) {
+                    MutableList(gridWithLoop.grid[0].size * 2) { '.' }
+                }.also { grid ->
+                    for (y in grid[0].indices) {
+                        for (x in grid.indices) {
+                            if (x % 2 == 0 && y % 2 == 0) {
+                                grid[x][y] = gridWithLoop.grid[x / 2][y / 2]
+                            } else {
+                                if (y % 2 == 0 && gridWithLoop.grid[x / 2][y / 2].connectsRight() && gridWithLoop.grid[x / 2 + 1][y / 2].connectsLeft()) {
+                                    grid[x][y] = '-'
+                                } else if (x % 2 == 0 && gridWithLoop.grid[x / 2][y / 2].connectsDown() && gridWithLoop.grid[x / 2][y / 2 + 1].connectsUp()) {
+                                    grid[x][y] = '|'
+                                }
+                            }
+                        }
+                    }
+                })
+
+            tailrec fun floodGrid(q: ArrayDeque<Point<Int>>, map: TubeMap) {
+                if (q.isEmpty()) return
+
+                q.removeFirst().surrounding()
+                    .filter { it.x >= 0 && it.y >= 0 && it.x < map.grid.size && it.y < map.grid[0].size }
+                    .filter { map.get(it) == '.' }.forEach {
+                        map.grid[it.x][it.y] = 'O'
+                        q.add(it)
+                    }
+                floodGrid(q, map)
+            }
+            floodGrid(ArrayDeque<Point<Int>>().also { it.add(0 by 0) }, expandedGrid)
+
+            var sum = 0
+            expandedGrid.grid.forEachIndexed { x, line ->
+                line.forEachIndexed { y, c ->
+                    if (x % 2 == 0 && y % 2 == 0 && c == '.') sum++
+                }
+            }
+
+            sum
         }
     }
 }
